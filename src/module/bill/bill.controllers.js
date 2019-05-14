5c7b8515bcd5092bad2c1befba2fbba3b43081ee
Quốc Khánh <bkdev98@gmail.com>
2019-05-14 18:34:10 +0700

    default store (home store)

--- src/module/bill/bill.controllers.js
+++ src/module/bill/bill.controllers.js
@@ -57,14 +57,16 @@ export async function returnToSupplier(req, res) {
         throw new Error('Invalid product!');
       }
       const store = await Store.findById({ _id: product.store, isRemoved: false });
-      product.quantity -= item.quantity;
-      //
-      product.total -= item.quantity;
-      //
-      store.productQuantity -= item.quantity;
-      store.returnedQuantity += item.quantity;
-      await product.save();
-      await store.save();
+      if (!store.isDefault) {
+        product.quantity -= item.quantity;
+        //
+        product.total -= item.quantity;
+        //
+        store.productQuantity -= item.quantity;
+        store.returnedQuantity += item.quantity;
+        await product.save();
+        await store.save();
+      }
     }));
     const bill = await Bill.createBill({
       productList,
@@ -87,39 +89,44 @@ export async function createBill(req, res) {
       }
       const store = await Store.findById({ _id: product.store, isRemoved: false });
 
-      // Trả hàng
-      if (item.isReturned) {
-        // Tìm hàng trả có sẵn
-        let returnedProduct = await Product.findOne({
-          importPrice: product.importPrice,
-          exportPrice: product.exportPrice,
-          isReturned: true,
-          isRemoved: false,
-        });
-        if (returnedProduct) {
-          returnedProduct.quantity += item.quantity;
-          returnedProduct.total += item.quantity;
-          await returnedProduct.save();
-        } else {
-          returnedProduct = await Product.createProduct({
+      if (!store.isDefault) {
+        // Trả hàng
+        if (item.isReturned) {
+          // Tìm hàng trả có sẵn
+          let returnedProduct = await Product.findOne({
             importPrice: product.importPrice,
             exportPrice: product.exportPrice,
-            store: product.store,
-            quantity: item.quantity,
-            total: item.quantity,
             isReturned: true,
-          }, req.user._id);
-        }
+            isRemoved: false,
+          });
+          if (returnedProduct) {
+            returnedProduct.quantity += item.quantity;
+            returnedProduct.total += item.quantity;
+            await returnedProduct.save();
+          } else {
+            returnedProduct = await Product.createProduct({
+              importPrice: product.importPrice,
+              exportPrice: product.exportPrice,
+              store: product.store,
+              quantity: item.quantity,
+              total: item.quantity,
+              isReturned: true,
+            }, req.user._id);
+          }
 
-        // product.quantity;
-        product.total -= item.quantity;
-        await product.save();
-        store.productQuantity += item.quantity;
+          // product.quantity;
+          product.total -= item.quantity;
+          await product.save();
+          store.productQuantity += item.quantity;
+        } else {
+          product.quantity -= item.quantity;
+          store.productQuantity -= item.quantity;
+          await product.save();
+        }
       } else {
-        product.quantity -= item.quantity;
-        store.productQuantity -= item.quantity;
-        await product.save();
+        // do with default store
       }
+
       return await store.save();
     }));
     const bill = await Bill.createBill(req.body, req.user._id);
