cd76b5c3fa28996b184495068ae41f97f6c1d1ea
Trần Tấn Đạt <37564125+dattt1807@users.noreply.github.com>
2020-03-05 01:22:56 +0700

    fix index

--- src/module/index.js
+++ src/module/index.js
@@ -1,15 +1,15 @@
 import userRoutes from './user/user.routes';
 import bookRoutes from './test/book.routes';
+import carModelRoutes from './carModel/carModel.routes';
 import carRoutes from './car/car.routes';
-import carmappingRoutes from './carmapping/carmapping.routes';
 import hubRoutes from './hub/hub.routes';
 import ratingRoutes from './rating/rating.routes';
 
 export default app => {
   app.use('/user', userRoutes);
   app.use('/book', bookRoutes);
+  app.use('/carModel', carModelRoutes);
   app.use('/car', carRoutes);
-  app.use('/carmapping', carmappingRoutes);
   app.use('/hub', hubRoutes);
   app.use('/rating', ratingRoutes);
 };
