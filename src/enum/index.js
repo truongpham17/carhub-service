2fc87087f5fb1f80e99b3b26dda83bdd451c3769
Trần Tấn Đạt <37564125+dattt1807@users.noreply.github.com>
2020-03-09 18:14:37 +0700

    Update module( model, validation, controllers), enum

--- src/enum/index.js
+++ src/enum/index.js
@@ -4,4 +4,14 @@ export default {
       ENUM: ['NONE', 'MALE', 'FEMALE'],
     },
   },
+  rental: {
+    status: {
+      ENUM: ['UPCOMING', 'CURRENT', 'OVERDUE', 'SHARING', 'SHARED', 'PAST'],
+    },
+  },
+  lease: {
+    status: {
+      ENUM: ['Pending', 'Available', 'Hiring', 'WAIT_TO_RETURN', 'PAST'],
+    },
+  },
 };
