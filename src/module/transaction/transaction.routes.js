cdde80ae46d340560654df281f8e8781df8f5786
Trần Tấn Đạt <37564125+dattt1807@users.noreply.github.com>
2020-03-23 18:01:56 +0700

    udpate Index

--- src/module/transaction/transaction.routes.js
+++ src/module/transaction/transaction.routes.js
@@ -15,7 +15,7 @@ const routes = new Router();
 routes.get('/', auth, getTransactionList);
 routes.get('/:id', auth, getTransaction);
 routes.post('/', auth, addTransaction);
-routes.delete('/', auth, deleteTransaction);
-// routes.patch('/:id', auth, validate(validation.updateLease), updateLease);
+routes.delete('/:id', auth, deleteTransaction);
+routes.put('/:id', auth, updateTransaction);
 
 export default routes;
